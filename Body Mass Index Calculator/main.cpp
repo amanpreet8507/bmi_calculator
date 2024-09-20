00ef7c1450ad56dc6dfee554cc98b3fac10f626d
Amanpreet Kaur <amanpreet8507@gmail.com>
2024-09-19 20:38:07 -0700

    Initial Commit

---
+++ Body Mass Index Calculator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Body Mass Index Calculator
+//
+//  Created by Amanpreeet Sandhu on 2024-09-19.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
