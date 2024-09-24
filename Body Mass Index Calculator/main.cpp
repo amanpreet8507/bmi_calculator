7b47761934e80a21e94eda29600bfbafc03ddb2b
Amanpreet Kaur <amanpreet8507@gmail.com>
2024-09-24 15:22:38 -0700

    bmi calculation feature

--- Body Mass Index Calculator/main.cpp
+++ Body Mass Index Calculator/main.cpp
@@ -7,8 +7,32 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+using std::endl;
+
+int main() {
+    
+    cout << "Enter your weight in pounds: ";
+    
+    // Declaration of variables
+    int weightInPounds;
+    int heightInInches;
+    int bmi;
+    
+    cin >> weightInPounds ; // Read user input
+    cout << endl  << "Enter your height in inches: ";
+    cin >> heightInInches; // Read user input
+    bmi = (weightInPounds * 703) / (heightInInches * heightInInches);
+    cout << endl << "BMI VALUES " << endl;
+    
+    // Output the BMI results as per user's BMI
+    if (bmi < 18.5) {
+        cout << "Underweight: less than 18.5" << endl << endl;
+    } else if (bmi >= 18.5 && bmi <= 24.9){
+        cout << "Normal : between 18.5 and 24.9" << endl << endl;
+    } else if (bmi >= 25 && bmi <= 29.9){
+        cout << "Overweight : between 25 and 29.9" << endl << endl;
+    } else{
+        cout << "Obese: 30 or greater" << endl << endl;
+    }
 }
